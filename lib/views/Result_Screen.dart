53e408190a555085f1942680f2aec120b6a3add6
Adham <adhamatef10@gmail.com>
2022-07-13 18:31:38 +0200

    BMI3

--- lib/views/Result_Screen.dart
+++ lib/views/Result_Screen.dart
@@ -1,55 +1,85 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class ReulstScreen extends StatelessWidget {
   final int result;
   final double wieght;
   final double height;
+  final String name;
+  // ignore: prefer_typing_uninitialized_variables
+  var age;
+  String? bmiStatues;
 
-  const ReulstScreen(
-      {Key? key,
-      required this.result,
-      required this.wieght,
-      required this.height})
+  ReulstScreen({Key? key,
+    required this.result,
+    required this.wieght,
+    required this.height,
+    required this.name,
+    required this.age
+  })
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    setBmiStatus();
     return Scaffold(
-      backgroundColor: Colors.pink[50],
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
+        iconTheme: const IconThemeData(color: Colors.black),
         backgroundColor: Colors.white,
-        title: Center(
+        title: const Center(
             child: Text(
-          'Result Screen',
-          style:
+              'Result Screen',
+              style:
               TextStyle(
-                  color: Colors.pink[600],
-                  fontWeight: FontWeight.bold,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
               ),
-        )),
+            )),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text(
+              'Your Name is : $name',
+              style: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Your Age is : $age',
+              style: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
               'Your Height is : $height',
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 25,
                 fontWeight: FontWeight.bold,
               ),
             ),
             Text(
               'Your Weight is :$wieght',
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 25,
                 fontWeight: FontWeight.bold,
               ),
             ),
             Text(
-              'Your Result is : $result',
-              style: TextStyle(
+              'Your BMI Result is : $result',
+              style: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Your Status is : $bmiStatues',
+              style: const TextStyle(
                 fontSize: 25,
                 fontWeight: FontWeight.bold,
               ),
@@ -58,5 +88,24 @@ class ReulstScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void setBmiStatus() {
+    if (result>30)
+      {
+        bmiStatues="Obese";
+      }
+    else if (result>=25)
+      {
+        bmiStatues="OverWeight";
+      }
+    else if (result>=18.5)
+      {
+        bmiStatues="Normal";
+      }
+    else if (result<18.5)
+      {
+        bmiStatues="UnderWeight";
+      }
   }
 }
